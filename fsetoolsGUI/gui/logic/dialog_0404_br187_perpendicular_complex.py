59a4b6f4091f0d35076cde509c9fe37ce0d969bd
fuyans <fuyans@gmail.com>
2020-03-08 12:05:03 +0000

    Separated background page and foreground figure as two labels.

--- fsetoolsGUI/gui/logic/dialog_0404_br187_perpendicular_complex.py
+++ fsetoolsGUI/gui/logic/dialog_0404_br187_perpendicular_complex.py
@@ -1,7 +1,8 @@
 from PySide2 import QtWidgets, QtGui, QtCore
 from fsetools.lib.fse_thermal_radiation import phi_perpendicular_any_br187, linear_solver
 
-from fsetoolsGUI.gui.images_base64 import dialog_0404_br187_perpendicular_figure_1 as figure_1
+from fsetoolsGUI.gui.images_base64 import dialog_0404_perpendicular_figure as image_figure
+from fsetoolsGUI.gui.images_base64 import dialog_0404_perpendicular_page as image_page
 from fsetoolsGUI.gui.layout.dialog_0403_br187_parallel_complex import Ui_MainWindow
 from fsetoolsGUI.gui.logic.OFRCustom import QMainWindow
 
@@ -19,18 +20,16 @@ class Dialog0404(QMainWindow):
         self.ui.setupUi(self)
         self.init()
 
-        from fsetoolsGUI.gui.logic.common import filter_objects_by_name
-        for i in filter_objects_by_name(self.ui.groupBox_out, object_types=[QtWidgets.QLineEdit]):
-            try:
-                i.setReadOnly(True)
-            except AttributeError:
-                i.setEnabled(False)
+        # from fsetoolsGUI.gui.logic.common import filter_objects_by_name
+        # for i in filter_objects_by_name(self.ui.groupBox_out, object_types=[QtWidgets.QLineEdit]):
+        #     try:
+        #         i.setReadOnly(True)
+        #     except AttributeError:
+        #         i.setEnabled(False)
 
         # set up radiation figure
-        ba = QtCore.QByteArray.fromBase64(figure_1)
-        pix_map = QtGui.QPixmap()
-        pix_map.loadFromData(ba)
-        self.ui.label.setPixmap(pix_map)
+        self.ui.label_image_figure.setPixmap(self.make_pixmap_from_base64(image_figure))
+        self.ui.label_image_page.setPixmap(self.make_pixmap_from_base64(image_page))
 
         self.ui.comboBox_S_or_UA.currentTextChanged.connect(self.change_mode_S_and_UA)
         self.ui.pushButton_calculate.clicked.connect(self.calculate)
