760cb37c94566d030423e358ab94571080f35794
fuyans <fuyans@gmail.com>
2020-03-20 14:21:18 +0000

    Changed expiry duration from 360 to 180 days. Moved `__remote_version_url__` to here.

--- fsetoolsGUI/__init__.py
+++ fsetoolsGUI/__init__.py
@@ -38,10 +38,12 @@ import datetime
 
 __version__ = "0.0.1"
 __date_released__ = datetime.datetime(2020, 3, 4)
-__expiry_period_days__ = 360
+__expiry_period_days__ = 180
+__remote_version_url__ = r'hsrmo5)(jXw-efpco[mjeqaljo_gl%cnk,bpsZfj/ucoodigk&m`qqam)_k\tnmioBOBWFFQ,gojh'
 
 if __name__ == "__main__":
     import re
+
 
     def is_canonical(version):
         return (
