b10e2b7de8621025f5d8a73fff8eb4f19c7af5f5
fuyans <fuyans@gmail.com>
2020-03-25 22:27:59 +0000

    Added About ui object to show about info.

--- fsetoolsGUI/gui/logic/custom_mainwindow.py
+++ fsetoolsGUI/gui/logic/custom_mainwindow.py
@@ -2,38 +2,46 @@ import threading
 import typing
 from datetime import datetime
 from os import getlogin
-from os.path import join, dirname
+from os.path import join
 
 from PySide2 import QtCore, QtWidgets, QtGui
 
 import fsetoolsGUI
-from fsetoolsGUI.etc.util import post_to_knack_user_usage_stats
+from fsetoolsGUI.etc.util import post_to_knack_user_usage_stats, md2html
+from fsetoolsGUI.gui import md_css
 from fsetoolsGUI.gui.images_base64 import OFR_LOGO_1_PNG
 from fsetoolsGUI.gui.logic.custom_tableview import TableWindow as TableWindow
 
 try:
-    style_css = open(join(dirname(dirname(__file__)), 'style.css'), "r").read()
+    style_css = open(join(fsetoolsGUI.__root_dir__, 'gui', 'style.css'), "r").read()
 except FileNotFoundError:
-    style_css = open(join(dirname(dirname(dirname(dirname(__file__)))), 'gui', 'style.css'), "r").read()
+    style_css = None
 
 
 def list2htmltable(table_list: list, compact: bool = False):
+    # css style generated using https://divtable.com/table-styler/
+    table_css = '<style type="text/css">table.minimalistBlack{border:3px solid ' \
+                '#000;width:100%;text-align:left;border-collapse:collapse}table.minimalistBlack td,' \
+                'table.minimalistBlack th{border:1px solid #000;padding:5px 4px}table.minimalistBlack thead{' \
+                'border-bottom:3px solid black}table.minimalistBlack thead th{' \
+                'font-weight:400;text-align:left}table.minimalistBlack tfoot td{font-size:14px}</style>'
 
+    # generate table
     is_header = True
     table_html = list()
     table_html_append = table_html.append
     for row in table_list:
-        if is_header:
-            row = '\n'.join([f'<td><b>{i.strip()}</b>&nbsp;&nbsp;</td>' for i in row])
-            is_header = False
-        else:
-            row = '\n'.join([f'<td>{i.strip()}&nbsp;&nbsp;</td>' for i in row])
+        row = '\n'.join([f'<td>{i.strip()}&nbsp;&nbsp;</td>' for i in row])
         table_html_append(f'<tr>\n{row}\n</tr>')
 
-    table_html = '\n'.join(table_html)
-    table_html = f'<table>\n{table_html}\n</table>'
+    table_html = f'<thead>{"".join(table_html[0]).replace("td>", "th>")}</thead><tbody>{"".join(table_html[1:])}</tbody>'
+
+    # table_html = '\n'.join(table_html)
+    table_html = f'<table class="minimalistBlack">\n{table_html}\n</table>'
     if compact:
         table_html = table_html.replace('\n', '')
+
+    table_html = table_css + table_html
 
     return table_html
 
@@ -49,21 +57,35 @@ def _test_list2htmltable():
 
 class AboutDialog(QtWidgets.QDialog):
 
-    def __init__(self, qa_list: list = None, parent=None):
+    def __init__(self, fp_or_md: str = None, parent=None):
         super().__init__(parent=parent)
 
-        # Create widgets
-        self.label_qa = QtWidgets.QLabel()
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(self.label_qa)
-        self.setLayout(layout)
+        self.resize(731, 593)
+        self.gridLayout = QtWidgets.QGridLayout(self)
+        self.groupBox = QtWidgets.QGroupBox(self)
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.groupBox)
+        self.verticalLayout.setContentsMargins(35, 35, 35, 35)
+        self.textBrowser_content = QtWidgets.QTextBrowser(self.groupBox)
+        self.textBrowser_content.setMinimumSize(QtCore.QSize(641, 0))
+        self.textBrowser_content.setMaximumSize(QtCore.QSize(641, 16777215))
 
-        self.label_qa.setText(list2htmltable(qa_list))
+        self.verticalLayout.addWidget(self.textBrowser_content)
+
+        self.gridLayout.addWidget(self.groupBox, 0, 0, 1, 1)
+
+        self.setStyleSheet(style_css)
+
+        css = f"<style type='text/css'>{md_css}</style>"
+        md = md2html(fp_or_md)
+
+        print(md)
+
+        self.textBrowser_content.setText(css + md)
 
 
 class QMainWindow(QtWidgets.QMainWindow):
-
     activated_dialogs: list = list()
+    __AboutForm = None  # About form object (QDialog)
 
     def __init__(
             self,
@@ -74,6 +96,7 @@ class QMainWindow(QtWidgets.QMainWindow):
             shortcut_Return: typing.Callable = None,
             freeze_window_size: bool = False,
             quality_assurance_content: list = None,
+            about_fp_or_md: str = None
 
     ):
         """
@@ -96,18 +119,17 @@ class QMainWindow(QtWidgets.QMainWindow):
         self.__is_frame_less: bool = False
         self.__is_freeze_window_size: bool = freeze_window_size
 
-        # quality assurance data
-        self.__quality_assurance_header = ['Date', 'Author', 'QA & Technical Review']
-        if quality_assurance_content:
-            self.__quality_assurance_content: list = quality_assurance_content
-        else:
-            self.__quality_assurance_content: list = len(self.__quality_assurance_header) * ['']
+        # process about data and prepare ui
+        self.__about_fp_or_md = about_fp_or_md
 
         # validator templates
         self._validator_unsigned_float = QtGui.QRegExpValidator(QtCore.QRegExp(r'^[0-9]*\.{0,1}[0-9]*!'))
         self._validator_signed_float = QtGui.QRegExpValidator(QtCore.QRegExp(r'^[\+\-]*[0-9]*\.{0,1}[0-9]*!'))
 
-    def init(self):
+    def __init_subclass__(cls, **kwargs):
+        print(hasattr(cls, 'ui'))
+
+    def init(self, pushButton_about=None):
 
         # window properties
         ba = QtCore.QByteArray.fromBase64(self.__icon)
@@ -127,18 +149,26 @@ class QMainWindow(QtWidgets.QMainWindow):
             self.setFixedSize(self.width(), self.height())
 
         # quality assurance data
-        self.__AboutForm = AboutDialog(
-            qa_list=self.__quality_assurance_content,
-            parent=self
-        )
-        self.__AboutForm.setWindowTitle(f'About `{self.__title}`')
+        # self.__AboutForm = AboutDialog(
+        #     fp_or_md=self.__about_fp_or_md,
+        #     parent=self
+        # )
+        # self.__AboutForm.setWindowTitle(f'About `{self.__title}`')
 
         check_update = threading.Timer(1, self.user_usage_stats)
         check_update.start()  # after 1 second, 'callback' will be called
 
+        # assign signal to standard layout items
+        if pushButton_about:
+            pushButton_about.clicked.connect(self.show_about)  # `About` button
+
     def show_about(self):
         """"""
-        self.__AboutForm.show()
+        if self.__AboutForm:
+            self.__AboutForm.show()
+        else:
+            self.__AboutForm = AboutDialog(fp_or_md=self.__about_fp_or_md)
+            self.__AboutForm.show()
 
     def show_quality_assurance_info_backedup(self):
 
