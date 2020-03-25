7aedd051b38e25f3bfce5fcdfd3ec799ce7ec517
fuyans <fuyans@gmail.com>
2020-03-25 22:29:26 +0000

    Added css style for markdown.

---
+++ fsetoolsGUI/gui/__init__.py
@@ -0,0 +1,5 @@
+md_css = 'table{border:3px solid ' \
+         '#000;width:100%;text-align:left;border-collapse:collapse}table td,' \
+         'table th{border:1px solid #000;padding:5px 4px}table thead{' \
+         'border-bottom:3px solid black}table thead th{' \
+         'font-weight:400;text-align:left}table tfoot td{font-size:14px}'
