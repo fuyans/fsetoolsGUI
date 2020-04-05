28d2fa364d805e862baac962df6991513465d6c4
fuyans <fuyans@gmail.com>
2020-04-05 22:00:29 +0100

    Finalised for Ieuan's comments.

--- fsetoolsGUI/gui/logic/dialog_0104_adb_merging_flow.py
+++ fsetoolsGUI/gui/logic/dialog_0104_adb_merging_flow.py
@@ -57,14 +57,20 @@ class Dialog0104(QMainWindow):
         # self.ui.lineEdit_in_N.setPlaceholderText('61')
 
         # set up validators
-        self.ui.lineEdit_in_W_SE.setValidator(self._validator_unsigned_float)
-        self.ui.lineEdit_in_S_up.setValidator(self._validator_unsigned_float)
-        self.ui.lineEdit_in_D.setValidator(self._validator_unsigned_float)
-        self.ui.lineEdit_in_N.setValidator(self._validator_unsigned_float)
+        # DEPRECIATED, NO GOOD TO IMPOSE RESTRICTIONS WITHOUT ALERTING USER
+        # self.ui.lineEdit_in_W_SE.setValidator(self._validator_unsigned_float)
+        # self.ui.lineEdit_in_S_up.setValidator(self._validator_unsigned_float)
+        # self.ui.lineEdit_in_D.setValidator(self._validator_unsigned_float)
+        # self.ui.lineEdit_in_N.setValidator(self._validator_unsigned_float)
 
         # signals
         self.ui.pushButton_ok.clicked.connect(self.calculate)
         self.ui.pushButton_example.clicked.connect(self.example)
+        self.ui.lineEdit_in_D.textChanged.connect(self.calculate)
+        self.ui.lineEdit_in_S_up.textChanged.connect(self.calculate)
+        self.ui.lineEdit_in_W_SE.textChanged.connect(self.calculate)
+        self.ui.lineEdit_in_N.textChanged.connect(self.calculate)
+
 
     def example(self):
         self.ui.lineEdit_in_D.setText('1.9')
@@ -74,24 +80,74 @@ class Dialog0104(QMainWindow):
 
         self.repaint()
 
-    def calculate(self):
+    @property
+    def input_parameters(self):
 
-        # clear ui output fields
-        self.ui.checkBox_out_check.setChecked(False)
-        self.ui.lineEdit_out_W_FE.setText('')
+        # ====================
+        # parse values from ui
+        # ====================
 
-        # parse inputs from ui
+        def str2float(v):
+            try:
+                return float(v)
+            except Exception as e:
+                return None
+
         try:
-            S_up = float(self.ui.lineEdit_in_S_up.text()) * 1e-3
-            W_SE = float(self.ui.lineEdit_in_W_SE.text()) * 1e-3
-            D = float(self.ui.lineEdit_in_D.text())
-            N = float(self.ui.lineEdit_in_N.text()) if self.ui.lineEdit_in_N.isEnabled() else None
+            S_up = str2float(self.ui.lineEdit_in_S_up.text())
+            if S_up is not None:
+                S_up /= 1e3
+            W_SE = str2float(self.ui.lineEdit_in_W_SE.text())
+            if W_SE is not None:
+                W_SE /= 1e3
+            D = str2float(self.ui.lineEdit_in_D.text())
+            N = str2float(self.ui.lineEdit_in_N.text())
         except Exception as e:
-            self.statusBar().showMessage(f'Failed to parse input. Error: {e}.')
-            self.repaint()
-            raise ValueError
+            raise e
 
-        # calculate
+        # =============================================
+        # check if enough input parameters are provided
+        # =============================================
+
+        try:
+            assert all([i is not None for i in [S_up, W_SE, D, N]])
+        except Exception:
+            raise ValueError('Not enough input parameters are provided')
+
+        # ====================================
+        # validate individual input parameters
+        # ====================================
+
+        if S_up is not None:
+            self.validate(S_up, 'unsigned float', 'Stair width to upper levels should be an unsigned float')
+        if W_SE is not None:
+            self.validate(W_SE, 'unsigned float', 'Door width from exit level should be an unsigned float')
+        if D is not None:
+            self.validate(D, 'unsigned float', 'Distance between doors should be an unsigned float')
+        if N is not None:
+            self.validate(N, 'unsigned float', 'No. of persons from final exit level should be an unsigned float')
+
+        return dict(S_up=S_up, D=D, N=N, W_SE=W_SE)
+
+    @property
+    def output_parameters(self):
+        return
+
+    @output_parameters.setter
+    def output_parameters(self, v):
+
+        try:
+            W_FE = v['W_FE']
+            condition_check = v['condition_check']
+        except KeyError:
+            raise KeyError('Not enough parameters to self.output_parameters')
+
+        self.ui.checkBox_out_check.setChecked(condition_check)
+        self.ui.lineEdit_out_W_FE.setText(f'{W_FE * 1e3:.1f}')
+
+    @staticmethod
+    def __calculate_merging_flow(S_up, D, N, W_SE):
+
         try:
             W_FE, condition_check = clause_2_23_merging_flow(
                 N=N,
@@ -100,14 +156,37 @@ class Dialog0104(QMainWindow):
                 W_SE=W_SE,
             )
         except Exception as e:
-            self.statusBar().showMessage(f'Calculation failed. Error: {e}')
-            self.repaint()
-            raise ValueError
+            raise e
 
-        self.ui.checkBox_out_check.setChecked(condition_check)
-        self.ui.lineEdit_out_W_FE.setText(f'{W_FE * 1e3:.1f}')
+        return dict(W_FE=W_FE, condition_check=condition_check)
 
-        self.statusBar().showMessage('Calculation complete.')
+    def calculate(self):
+
+        # clear ui output fields
+        self.ui.checkBox_out_check.setChecked(False)
+        self.ui.lineEdit_out_W_FE.clear()
+
+        # parse inputs from ui
+        try:
+            input_parameters = self.input_parameters
+        except Exception as e:
+            self.statusBar().showMessage(f'Unable to parse input parameters. {str(e)}')
+            return
+
+        # calculate
+        try:
+            output_parameters = self.__calculate_merging_flow(**input_parameters)
+        except Exception as e:
+            self.statusBar().showMessage(f'Calculation error. {str(e)}')
+            return
+
+        # results to ui
+        try:
+            self.output_parameters = output_parameters
+        except Exception as e:
+            self.statusBar().showMessage(f'Unable to cast results to UI. {str(e)}')
+            return
+
         self.repaint()
 
 
--- fsetoolsGUI/gui/logic/dialog_0601_naming_convention.py
+++ fsetoolsGUI/gui/logic/dialog_0601_naming_convention.py
@@ -1,15 +1,17 @@
+import re
 from datetime import datetime
-from os.path import join
 
-from PySide2 import QtCore, QtGui
+from PySide2 import QtGui
 
-import fsetoolsGUI
 from fsetoolsGUI.gui.layout.dialog_0601_naming_convention import Ui_MainWindow
 from fsetoolsGUI.gui.logic.custom_mainwindow import QMainWindow
 
 
 class Dialog0601(QMainWindow):
-    fp_doc = join(fsetoolsGUI.__root_dir__, 'gui', 'docs', '0601.md')  # doc file path
+    __re_date = re.compile(r'[0-9]{4,6}')
+    __re_project_no = re.compile(r'[a-zA-Z]{2}[0-9]{5,6}')
+    __re_project_stage = re.compile(r'.+')
+    __re_project_title = re.compile(r'.+')
 
     def __init__(self, parent=None):
         # init
@@ -37,44 +39,113 @@ class Dialog0601(QMainWindow):
         self.ui.lineEdit_5_title.setPlaceholderText('Fire safety strategy')
 
         # validators
-        self.ui.lineEdit_1_date.setValidator((QtGui.QRegExpValidator(QtCore.QRegExp(r'^[0-9]{6,8}'))))
-        self.ui.lineEdit_3_project_no.setValidator(QtGui.QRegExpValidator(QtCore.QRegExp(r'^[A-Z]{1,2}[0-9]{1,5}')))
-        self.ui.lineEdit_4_project_stage.setValidator(QtGui.QRegExpValidator(QtCore.QRegExp(r'^[\w\-. ]+$')))
-        self.ui.lineEdit_5_title.setValidator(QtGui.QRegExpValidator(QtCore.QRegExp(r'^[\w\-. ]+$')))
+        # self.ui.lineEdit_1_date.setValidator((QtGui.QRegExpValidator(QtCore.QRegExp(r'^[0-9]{6,8}'))))
+        # self.ui.lineEdit_3_project_no.setValidator(QtGui.QRegExpValidator(QtCore.QRegExp(r'^[A-Z]{1,2}[0-9]{1,5}')))
+        # self.ui.lineEdit_4_project_stage.setValidator(QtGui.QRegExpValidator(QtCore.QRegExp(r'^[\w\-. ]+$')))
+        # self.ui.lineEdit_5_title.setValidator(QtGui.QRegExpValidator(QtCore.QRegExp(r'^[\w\-. ]+$')))
 
         # signal and slots
-        self.ui.lineEdit_1_date.textChanged.connect(self.make_file_name)
-        self.ui.comboBox_2_revision.currentTextChanged.connect(self.make_file_name)
-        self.ui.lineEdit_3_project_no.textChanged.connect(self.make_file_name)
-        self.ui.lineEdit_4_project_stage.textChanged.connect(self.make_file_name)
-        self.ui.lineEdit_5_title.textChanged.connect(self.make_file_name)
-        self.ui.comboBox_6_type.currentTextChanged.connect(self.make_file_name)
-        self.ui.comboBox_7_security_status.currentTextChanged.connect(self.make_file_name)
-        self.ui.checkBox_replace_spaces.stateChanged.connect(self.make_file_name)
+        self.ui.lineEdit_1_date.textChanged.connect(self.calculate)
+        self.ui.comboBox_2_revision.currentTextChanged.connect(self.calculate)
+        self.ui.lineEdit_3_project_no.textChanged.connect(self.calculate)
+        self.ui.lineEdit_4_project_stage.textChanged.connect(self.calculate)
+        self.ui.lineEdit_5_title.textChanged.connect(self.calculate)
+        self.ui.comboBox_6_type.currentTextChanged.connect(self.calculate)
+        self.ui.comboBox_7_security_status.currentTextChanged.connect(self.calculate)
+        self.ui.checkBox_replace_spaces.stateChanged.connect(self.calculate)
         self.ui.pushButton_ok.clicked.connect(self.copy_file_name)
 
         # clean up
-        self.make_file_name()  # make file name, do not leave the output slot empty
+        self.calculate()  # make file name, do not leave the output slot empty
         self.ui.pushButton_ok.setText('Copy')
         self.ui.pushButton_ok.setToolTip('Click (or press Enter) to copy the generated file name')
         self.repaint()
 
-    def make_file_name(self):
-        aa = self.ui.lineEdit_1_date.text()
-        bb = self.ui.comboBox_2_revision.currentText()[0:3]
-        cc = self.ui.lineEdit_3_project_no.text()
-        dd = self.ui.lineEdit_4_project_stage.text()
-        ee = self.ui.lineEdit_5_title.text()
-        ff = self.ui.comboBox_6_type.currentText()[0:2]
-        gg = self.ui.comboBox_7_security_status.currentText()[0:3]
+    @property
+    def input_parameters(self) -> dict:
 
+        # ====================
+        # parse values from ui
+        # ====================
+        date = self.ui.lineEdit_1_date.text()
+        revision = self.ui.comboBox_2_revision.currentText()[0:3]  # obtain the first three letters/digits, i.e. D00
+        project_no = self.ui.lineEdit_3_project_no.text().upper()  # capitalise office designation letters
+        project_stage = self.ui.lineEdit_4_project_stage.text()
+        title = self.ui.lineEdit_5_title.text()
+        type = self.ui.comboBox_6_type.currentText()[0:2]
+        security_status = self.ui.comboBox_7_security_status.currentText()[0:3]
+
+        # =====================
+        # validate input values
+        # =====================
+        if self.__re_date.match(date) is None:
+            raise ValueError('Date should be in format YYMMDD or YYYYMMDD')
+        if self.__re_project_no.match(project_no) is None:
+            raise ValueError('Project number should be in format XX#####, '
+                             'where XX is office designation and # is a project number')
+        if self.__re_project_stage.match(project_stage) is None:
+            raise ValueError('Project stage is missing')
+        if self.__re_project_title.match(title) is None:
+            raise ValueError('Project title is missing')
+
+        return dict(date=date, revision=revision, project_no=project_no, project_stage=project_stage, title=title,
+                    type=type, security_status=security_status)
+
+    def calculate(self):
+
+        # ======================
+        # parse input parameters
+        # ======================
+        try:
+            input_parameters = self.input_parameters
+        except Exception as e:
+            self.statusBar().showMessage(f'{e}')
+            return
+        try:
+            date = input_parameters['date']
+            revision = input_parameters['revision']
+            project_no = input_parameters['project_no']
+            project_stage = input_parameters['project_stage']
+            title = input_parameters['title']
+            type = input_parameters['type']
+            security_status = input_parameters['security_status']
+        except KeyError:
+            self.statusBar().showMessage('Missing input parameters from `self.input_parameters`')
+            return
+
+        # ==============
+        # make file name
+        # ==============
+        file_name = '-'.join([date, revision, project_no, project_stage, title, type, security_status])
         if self.ui.checkBox_replace_spaces.isChecked():
-            dd = dd.replace(' ', '_')
-            ee = ee.replace(' ', '_')
+            file_name = file_name.replace(' ', '_')
+        output_parameters = dict(file_name=file_name)
 
-        self.ui.lineEdit_out_result.setText('-'.join([aa, bb, cc, dd, ee, ff, gg]))
+        # ==================
+        # cast results to ui
+        # ==================
+        try:
+            self.output_parameters = output_parameters
+        except Exception as e:
+            self.statusBar().showMessage(f'{e}')
+
+        self.ui.lineEdit_out_result.selectAll()
+        self.ui.statusbar.showMessage('File name is copied.')
 
         self.repaint()
+
+    @property
+    def output_parameters(self):
+        return
+
+    @output_parameters.setter
+    def output_parameters(self, v):
+        try:
+            file_name = v['file_name']
+        except KeyError:
+            raise KeyError('Not enough output parameters provided to self.output_parameters')
+
+        self.ui.lineEdit_out_result.setText(file_name)
 
     def copy_file_name(self):
         clipboard = QtGui.QGuiApplication.clipboard()
