f22b9dbda32db160ed392c678436e3e8b583152d
fuyans <fuyans@gmail.com>
2020-06-29 08:44:46 +0100

    Finalised 0630, added batch populate bc file. (#4)

--- fsetoolsGUI/gui/logic/c0630_safir_post_processor.py
+++ fsetoolsGUI/gui/logic/c0630_safir_post_processor.py
@@ -1,6 +1,14 @@
-import os.path as path
+import copy
+import logging
+import multiprocessing as mp
+import os
+import subprocess
 import threading
+import time
+from os import path
+from typing import List, Dict, Callable
 
+import numpy as np
 from PySide2 import QtWidgets, QtCore
 from PySide2.QtCore import Slot
 
@@ -10,13 +18,74 @@ from fsetoolsGUI.gui.logic.custom_app_template import AppBaseClass
 from fsetoolsGUI.gui.logic.custom_plot import App as PlotApp
 from fsetoolsGUI.gui.logic.custom_table import TableWindow
 
+logger = logging.getLogger('gui')
+
+
+def safir_batch_run_worker(args: List) -> List:
+    def worker(
+            cmd: str,
+            cwd: str,
+            fp_stdout: str = None,
+            timeout_seconds: int = 1 * 60,
+    ) -> List:
+        try:
+            if fp_stdout:
+                subprocess.call(cmd, cwd=cwd, timeout=timeout_seconds, stdout=open(fp_stdout, 'w+'))
+            else:
+                subprocess.call(cmd, cwd=cwd, timeout=timeout_seconds, stdout=open(os.devnull, 'w'))
+            return [cmd, 'Success']
+        except subprocess.TimeoutExpired:
+            return [cmd, 'Timed out']
+
+    kwargs, q = args
+    result = worker(**kwargs)
+    q.put(1)
+    return result
+
+
+def safir_batch_copy_bc_to_input_folder(fp_bc: str, ):
+    pass
+
 
 class Signals(QtCore.QObject):
     __process_safir_out_file_complete = QtCore.Signal(bool)
+    __progress_batch_run = QtCore.Signal(int)
 
     @property
     def process_safir_out_file_complete(self) -> QtCore.Signal:
         return self.__process_safir_out_file_complete
+
+    @property
+    def progress_batch_run(self) -> QtCore.Signal:
+        return self.__progress_batch_run
+
+
+class ProgressBar(QtWidgets.QDialog):
+    def __init__(self, title: str = None, initial_value: int = 0, parent=None):
+        super().__init__(parent)
+        self.setWindowFlag(QtCore.Qt.WindowContextHelpButtonHint, False)
+
+        self.setWindowTitle(title)
+        self.setSizeGripEnabled(False)
+
+        self.progressbar = QtWidgets.QProgressBar()
+        self.progressbar.setMinimum(0)
+        self.progressbar.setMaximum(100)
+        self.progressbar.setValue(initial_value)
+
+        self.button_cancel = QtWidgets.QPushButton("Cancel")
+
+        self.grid_layout = QtWidgets.QGridLayout()
+        self.grid_layout.addWidget(self.progressbar, 1, 0)
+        self.grid_layout.addWidget(self.button_cancel, 1, 1)
+
+        self.setLayout(self.grid_layout)
+
+        self.resize(300, 50)
+
+    @QtCore.Slot(int)
+    def update_progress_bar(self, progress: int):
+        self.progressbar.setValue(progress)
 
 
 class App(AppBaseClass):
@@ -25,6 +94,8 @@ class App(AppBaseClass):
     app_name_long = 'Safir post processor'
 
     def __init__(self, parent=None, mode=None):
+        super().__init__(parent=parent)
+
         self.__dict_out = None
         self.__Table = None
         self.__Figure = None
@@ -35,11 +106,11 @@ class App(AppBaseClass):
         self.__strain_lines = None
         self.__Signals = Signals()
         self.__output_fire_curve = dict(time=None, temperature=None)
+        self.__progress_bar = ProgressBar('Progress', parent=self, initial_value=0)
 
         # ================================
         # instantiation super and setup ui
         # ================================
-        super().__init__(parent=parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.init()
@@ -62,43 +133,180 @@ class App(AppBaseClass):
         self.ui.pushButton_fp_out.clicked.connect(self.__upon_output_file_selection_step_1)
         self.ui.comboBox_in_shell.currentIndexChanged.connect(self.__upon_shell_combobox_change)
 
-    @property
-    def input_parameters(self):
+        self.init_batch_run()
+        self.init_batch_bc()
 
-        def str2int(v):
+        self.__Signals.progress_batch_run.connect(self.__progress_bar.update_progress_bar)
+
+    def init_batch_run(self):
+
+        def select_safir_exe_path():
             try:
-                return int(v)
-            except:
-                return None
+                default_dir = path.dirname(path.realpath(self.ui.lineEdit_batch_run_in_safir_exe_path.text()))
+                if not (path.exists(default_dir) and path.isdir(default_dir)):
+                    default_dir = '~/'
+            except Exception as e:
+                logger.error(f'Unable to get old dir {e}')
+                default_dir = '~/'
 
-        # ====================
-        # parse values from ui
-        # ====================
-        fp_out = self.ui.lineEdit_in_fp_out.text()
-        unique_shell = str2int(self.ui.lineEdit_in_shell.text())
+            fp = self.select_file_path(title='Select Safir executable', default_dir=default_dir,
+                                       file_type="Safir executable (*.exe)")
+            if fp:
+                self.ui.lineEdit_batch_run_in_safir_exe_path.setText(fp)
 
-        # ======================================================
-        # check if necessary inputs are provided for calculation
-        # ======================================================
+        def select_safir_input_root_folder():
+            try:
+                default_dir = path.realpath(self.ui.lineEdit_batch_run_in_safir_input_folder.text())
+                if not (path.exists(default_dir) and path.isdir(default_dir)):
+                    default_dir = '~/'
+            except Exception as e:
+                logger.error(f'Unable to get old dir {e}')
+                default_dir = '~/'
+            fp = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select folder', default_dir)
+            if fp:
+                self.ui.lineEdit_batch_run_in_safir_input_folder.setText(fp)
 
-        # ==============================
-        # validate individual parameters
-        # ==============================
+        def batch_run():
+            try:
+                fp_safir_exe = self.ui.lineEdit_batch_run_in_safir_exe_path.text()
+                dir_work = self.ui.lineEdit_batch_run_in_safir_input_folder.text()
+                timeout_seconds = int(self.ui.lineEdit_batch_run_in_timeout.text())
 
-        # ================
-        # units conversion
-        # ================
+                list_fp_in = list()
+                for root, dirs, files in os.walk(dir_work):
+                    for file_ in files:
+                        if file_.endswith('.in'):
+                            list_fp_in.append(path.join(root, file_))
 
-        return dict(fp_out=fp_out, unique_shell=unique_shell)
+                list_kwargs_in = list()
+                for i in list_fp_in:
+                    list_kwargs_in.append(dict(
+                        cmd=[fp_safir_exe, path.basename(i).replace('.in', '')],
+                        cwd=path.dirname(i),
+                        fp_stdout=path.join(path.dirname(i), path.basename(i).replace('.in', '.stdout.txt')),
+                        timeout_seconds=timeout_seconds,
+                    ))
 
-    @property
-    def output_parameters(self):
-        return self.__output_fire_curve
+                kwargs = dict(
+                    list_kwargs_in=list_kwargs_in,
+                    func_mp=safir_batch_run_worker,
+                    n_proc=int(self.ui.lineEdit_batch_run_in_processes.text()) or 1,
+                    dir_work=dir_work
+                )
+                t = threading.Thread(target=self.safir_batch_run, kwargs=kwargs)
+                t.start()
 
-    @output_parameters.setter
-    def output_parameters(self, v):
-        self.__output_fire_curve['time'] = v['time']
-        self.__output_fire_curve['temperature'] = v['temperature']
+                self.__progress_bar.show()
+
+            except Exception as e:
+                self.statusBar().showMessage(f'Simulation failed {e}')
+
+        self.ui.pushButton_batch_run_in_safir_exe_path.clicked.connect(select_safir_exe_path)
+        self.ui.pushButton_batch_run_in_safir_input_folder.clicked.connect(select_safir_input_root_folder)
+        self.ui.pushButton_batch_run_submit.clicked.connect(batch_run)
+
+    def init_batch_bc(self):
+        def select_root_dir():
+            fp = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select folder')
+            if fp:
+                fp = path.realpath(fp)
+                self.ui.lineEdit_batchbc_root_dir.setText(fp)
+
+                list_fp = list()
+                for root, dirs, files in os.walk(fp):
+                    for file_ in files:
+                        if file_.endswith('.in'):
+                            relative_path = path.realpath(root).lstrip(fp)
+                            # relative_path.lstrip(fp)
+                            print(relative_path, fp)
+                            list_fp.append(relative_path)
+                list_fp = list(set(list_fp))
+                list_fp = ['rel_dir,val'] + [f'{i},{j}' for i, j in zip(list_fp, [1] * len(list_fp))]
+
+                with open(path.join(fp, 'bc_reduction_template.csv'), 'w+') as f:
+                    f.write('\n'.join(list_fp))
+
+        def select_bc_file():
+            fp, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Select bc file')
+            if fp:
+                self.ui.lineEdit_batchbc_bc_file.setText(fp)
+
+        def run():
+            dir_work = self.ui.lineEdit_batchbc_root_dir.text()
+            fp_bc = self.ui.lineEdit_batchbc_bc_file.text()
+            is_apply_reduction_factor = self.ui.checkBox_batchbc_reduction_factor.isChecked()
+
+            bc = np.genfromtxt(fp_bc, delimiter=',')
+
+            list_dir = list()
+            for root, dirs, files in os.walk(dir_work):
+                for file_ in files:
+                    if file_.endswith('.in'):
+                        list_dir.append(path.realpath(root))
+
+            dict_reduction = dict()
+            if is_apply_reduction_factor:
+                for i, j in np.recfromcsv(path.join(dir_work, 'bc_reduction.csv'), autostrip=True, dtype=None):
+                    k = i.decode('utf-8')
+                    dict_reduction[k] = j
+
+            for dir in list_dir:
+                if dict_reduction:
+                    k = dir.lstrip(dir_work)
+                    print(k, list(dict_reduction.keys()))
+                    reduction_value = dict_reduction[k]
+                    bc_ = copy.copy(bc)
+                    bc_[:, 1] = bc_[:, 1] * reduction_value
+                else:
+                    bc_ = bc
+                np.savetxt(path.join(dir, path.basename(fp_bc)), bc_, delimiter=',', fmt='%g')
+
+        self.ui.pushButton_batchbc_root_dir.clicked.connect(select_root_dir)
+        self.ui.pushButton_batchbc_bc_file.clicked.connect(select_bc_file)
+        self.ui.pushButton_batchbc_ok.clicked.connect(run)
+        pass
+
+    def safir_batch_run(self, list_kwargs_in: List[Dict], func_mp: Callable, n_proc: int = 1, dir_work: str = None, ):
+        # ------------------------------------------
+        # prepare variables used for multiprocessing
+        # ------------------------------------------
+        m, p = mp.Manager(), mp.Pool(n_proc, maxtasksperchild=1000)
+        q = m.Queue()
+        jobs = p.map_async(func_mp, [(dict_, q) for dict_ in list_kwargs_in])
+        n_simulations = len(list_kwargs_in)
+
+        # ---------------------
+        # multiprocessing start
+        # ---------------------
+        while True:
+            if jobs.ready():
+                self.__Signals.progress_batch_run.emit(100)
+                break  # complete
+            else:
+                self.__Signals.progress_batch_run.emit(int(q.qsize() / n_simulations * 100))
+                time.sleep(1)  # in progress
+
+        # --------------------------------------------
+        # pull results and close multiprocess pipeline
+        # --------------------------------------------
+        print('d')
+        p.close()
+        p.join()
+        mp_out = jobs.get()
+        time.sleep(0.5)
+
+        # ----------------------
+        # save and print summary
+        # ----------------------
+        if dir_work:
+            out = mp_out
+            len_1 = int(max([len(' '.join(i[0])) for i in out]))
+            summary = '\n'.join([f'{" ".join(i[0]):<{len_1}} - {i[1]:<{len_1}}' for i in out])
+            print(summary)
+            with open(path.join(dir_work, 'summary.txt'), 'w+') as f:
+                f.write(summary)
+
+        return mp_out
 
     def __upon_output_file_selection_step_1(self):
         self.statusBar().showMessage('Processing *.out file ...')
@@ -197,9 +405,9 @@ class App(AppBaseClass):
 
     def __upon_shell_combobox_change(self):
         self.ui.lineEdit_in_shell.setText(self.ui.comboBox_in_shell.currentText())
-        self.ok_silent()
+        self.post_strain_ok_slient()
 
-    def ok_silent(self):
+    def post_strain_ok_slient(self):
 
         # --------------------
         # Parse inputs from UI
@@ -235,9 +443,9 @@ class App(AppBaseClass):
 
         return 0
 
-    def ok(self):
+    def post_strain_ok(self):
 
-        res = self.ok_silent()
+        res = self.post_strain_ok_slient()
 
         if isinstance(res, Exception):
             self.statusBar().showMessage(f'{res}')
@@ -245,15 +453,12 @@ class App(AppBaseClass):
     def make_figure_and_table(self, unique_shell: int, **kwargs):
         self.__strain_lines = make_strain_lines_for_given_shell(**kwargs)
 
-    def select_file_path(self):
+    def select_file_path(self, title: str = "Select file", default_dir: str = "~/",
+                         file_type: str = "Safir output file (*.out *.OUT *.txt)"):
         """select input file and copy its path to ui object"""
 
         # dialog to select file
-        path_to_file, _ = QtWidgets.QFileDialog.getOpenFileName(
-            self,
-            "Select File",
-            "~/",
-            "Safir output file (*.out *.OUT *.txt)")
+        path_to_file, _ = QtWidgets.QFileDialog.getOpenFileName(self, title, default_dir, file_type)
 
         # paste the select file path to the ui object
         return path_to_file
@@ -338,6 +543,47 @@ class App(AppBaseClass):
 
         return True
 
+    def ok(self):
+        pass
+
+    @property
+    def input_parameters(self):
+
+        def str2int(v):
+            try:
+                return int(v)
+            except:
+                return None
+
+        # ====================
+        # parse values from ui
+        # ====================
+        fp_out = self.ui.lineEdit_in_fp_out.text()
+        unique_shell = str2int(self.ui.lineEdit_in_shell.text())
+
+        # ======================================================
+        # check if necessary inputs are provided for calculation
+        # ======================================================
+
+        # ==============================
+        # validate individual parameters
+        # ==============================
+
+        # ================
+        # units conversion
+        # ================
+
+        return dict(fp_out=fp_out, unique_shell=unique_shell)
+
+    @property
+    def output_parameters(self):
+        return self.__output_fire_curve
+
+    @output_parameters.setter
+    def output_parameters(self, v):
+        self.__output_fire_curve['time'] = v['time']
+        self.__output_fire_curve['temperature'] = v['temperature']
+
 
 if __name__ == "__main__":
     import sys
@@ -345,4 +591,6 @@ if __name__ == "__main__":
     qapp = QtWidgets.QApplication(sys.argv)
     app = App()
     app.show()
+    # app2 = ProgressBar('Progress bar')
+    # app2.show()
     qapp.exec_()
--- fsetoolsGUI/gui/logic/custom_app_template.py
+++ fsetoolsGUI/gui/logic/custom_app_template.py
@@ -1,17 +1,13 @@
 import logging
 import threading
-import typing
 from datetime import datetime
 from os import getlogin, path
 
 from PySide2 import QtCore, QtWidgets, QtGui
 
-# import fsetoolsGUI
 from fsetoolsGUI import __root_dir__, __version__
-# from fsetoolsGUI import AppInfo
 from fsetoolsGUI.etc.util import post_to_knack_user_usage_stats
 from fsetoolsGUI.gui.layout.i0001_text_browser import Ui_MainWindow
-
 
 logger = logging.getLogger('gui')
 
@@ -112,7 +108,8 @@ class AppBaseClass(QtWidgets.QMainWindow):
             self.ui.pushButton_ok.clicked.connect(self.ok)
         if hasattr(self.ui, 'pushButton_about'):
             if self.__about_dialog is None:
-                self.__about_dialog = AboutDialog(fp_or_html=path.join(__root_dir__, 'gui', 'docs', f'{self.app_id}.html'))
+                self.__about_dialog = AboutDialog(
+                    fp_or_html=path.join(__root_dir__, 'gui', 'docs', f'{self.app_id}.html'))
             self.ui.pushButton_about.clicked.connect(lambda: self.__about_dialog.show())
         if hasattr(self.ui, 'pushButton_example'):
             self.ui.pushButton_example.clicked.connect(self.example)
@@ -149,10 +146,6 @@ class AppBaseClass(QtWidgets.QMainWindow):
         msgbox.setText(msg)
         msgbox.setStandardButtons(msgbox.Ok)
         msgbox.exec_()
-    #
-    # @property
-    # def module_id(self) -> str:
-    #     return self.__id
 
     @property
     def activated_dialogs(self):
@@ -202,242 +195,6 @@ class AppBaseClass(QtWidgets.QMainWindow):
             except AssertionError:
                 raise ValueError(err_msg)
 
-#
-# class QMainWindow(QtWidgets.QMainWindow):
-#     """todo: docstring"""
-#
-#     def __init__(
-#             self,
-#             module_id: str,
-#             icon: typing.Union[bytes, QtCore.QByteArray, QtGui.QPixmap] = None,
-#             title: str = None,
-#             parent=None,
-#             freeze_window_size: bool = False,
-#             about_fp_or_md: str = None,
-#             mode=None
-#     ):
-#         """
-#         todo: docstring
-#         """
-#
-#         self.__activated_dialogs: list = list()
-#         self.__about_form = None  # About ui
-#
-#         # ============================
-#         # set default input parameters
-#         # ============================
-#         # make icon object
-#         if icon is None:
-#             icon = QtGui.QPixmap(path.join(__root_dir__, 'gui', 'icons', 'LOGO_1_80_80.png'))
-#         else:
-#             if isinstance(icon, bytes) or isinstance(icon, QtCore.QByteArray):
-#                 bm = QtCore.QByteArray.fromBase64(QtCore.QByteArray(icon))
-#                 pixmap = QtGui.QPixmap()
-#                 pixmap.loadFromData(bm)
-#                 icon = pixmap
-#
-#         # =================
-#         # instantiate super
-#         # =================
-#         super().__init__(parent=parent)
-#
-#         self.AppInfo = AppInfo(int(module_id))
-#         self.Validator = Validator()
-#
-#         # =================
-#         # create properties
-#         # =================
-#         self.__id: str = module_id
-#         self.__title: str = title
-#         self.__icon: bytes = icon
-#         self.__is_frame_less: bool = False
-#         self.__is_freeze_window_size: bool = freeze_window_size
-#         self.__about_fp_or_md = about_fp_or_md  # process about data and prepare ui
-#         self.__mode = mode
-#
-#     def init(self, cls):
-#         pass
-#
-#
-#     def validate_show_statusBar_msg(self, var, type, err_msg: str):
-#         try:
-#             self.validate(var, type, err_msg)
-#         except Exception as e:
-#             self.statusBar().showMessage(f'{e}')
-#
-#     @staticmethod
-#     def validate(var, type, err_msg: str):
-#         if type == 'unsigned float':
-#             try:
-#                 assert isinstance(var, float)
-#                 assert var >= 0
-#             except AssertionError:
-#                 raise ValueError(err_msg)
-#         else:
-#             try:
-#                 assert isinstance(var, type)
-#             except AssertionError:
-#                 raise ValueError(err_msg)
-#
-#     @staticmethod
-#     def make_pixmap_from_base64(image_base64: bytes):
-#         ba = QtCore.QByteArray.fromBase64(QtCore.QByteArray(image_base64))
-#         pix_map = QtGui.QPixmap()
-#         pix_map.loadFromData(ba)
-#         return pix_map
-#
-#     @staticmethod
-#     def make_pixmap_from_fp(fp: str):
-#         return QtGui.QPixmap(fp)
-#
-#     def closeEvent(self, event):
-#         if self.__about_form is not None:
-#             self.__about_form.close()
-#         event.accept()
-#
-#     def message_box(self, msg: str, title: str):
-#         msgbox = QtWidgets.QMessageBox(parent=self)
-#         msgbox.setIconPixmap(path.join(fsetoolsGUI.__root_dir__, 'gui', 'images', 'LOGO_1_80_80.PONG'))
-#         msgbox.setWindowTitle(title)
-#         msgbox.setText(msg)
-#         msgbox.setStandardButtons(msgbox.Ok)
-#         msgbox.exec_()
-#
-#     @property
-#     def module_id(self) -> str:
-#         return self.__id
-#
-#     def copy_str(self, s: str):
-#         clipboard = QtGui.QGuiApplication.clipboard()
-#         clipboard.setText(s)
-#
-#     @property
-#     def activated_dialogs(self):
-#         return self.__activated_dialogs
-#
-#     @activated_dialogs.setter
-#     def activated_dialogs(self, d):
-#         self.__activated_dialogs.append(d)
-#
-#     def init_old(self, cls):
-#         # -----------------
-#         # window properties
-#         # -----------------
-#         self.setWindowIcon(self.__icon)
-#
-#         # set window title
-#         if self.__title:
-#             self.setWindowTitle(self.__title)
-#         else:
-#             self.setWindowTitle(self.AppInfo.long_name)
-#
-#         self.setStyleSheet(qt_css)
-#         self.statusBar().setSizeGripEnabled(False)
-#
-#         self.adjustSize()
-#
-#         if self.__is_freeze_window_size:
-#             self.statusBar().setSizeGripEnabled(False)
-#             self.setFixedSize(self.width(), self.height())
-#
-#         # ------------
-#         # check update
-#         # ------------
-#         if self.__mode != -1:
-#             try:
-#                 check_update = threading.Timer(1, self.__user_usage_stats)
-#                 check_update.start()  # after 1 second, 'callback' will be called
-#             except Exception as e:
-#                 logger.error(f'{str(e)}')
-#
-#         # --------------------------------------
-#         # assign signal to standard layout items
-#         # --------------------------------------
-#         def set_action_name_and_tip(
-#                 btncls,
-#                 action: callable = None,
-#                 name: str = '',
-#                 tooltip: str = '',
-#                 statustip: str = None):
-#             if action:
-#                 btncls.clicked.connect(action)
-#             btncls.setText(name)
-#             btncls.setToolTip(tooltip)
-#             btncls.setStatusTip(statustip)
-#
-#         if hasattr(cls.ui, 'pushButton_about'):
-#             set_action_name_and_tip(cls.ui.pushButton_about, self.about, 'About',
-#                                     'Click to show info about this app and quality management')
-#         if hasattr(cls.ui, 'pushButton_ok'):
-#             set_action_name_and_tip(cls.ui.pushButton_ok, self.ok, 'OK', 'Click (or press Enter) to calculate')
-#         if hasattr(cls.ui, 'pushButton_example'):
-#                 set_action_name_and_tip(cls.ui.pushButton_example, self.example, 'Example',
-#                                         'Click to show example input parameters')
-#
-#     def ok(self):
-#         """Placeholder method to be overridden by child classes.
-#         This method is expected to be triggered upon clicking the 'OK' or 'Calculate' button. The following comments
-#         are suggested procedure to be followed. This method is also connected by keyboard shortcut 'Enter'"""
-#
-#         # Step 1. Get parameters from UI
-#         # Step 2. Perform analysis
-#         # Step 3. Cast result onto UI
-#
-#         pass
-#
-#     def example(self):
-#         """Placeholder method to be overriden by child classes.
-#         This method is expected to be triggered upon clicking the 'Example' button. The following comments are suggested
-#         procedures to be followed."""
-#
-#         # Step 1. Prepare and cast example input parameters onto the UI
-#         # Step 2. Call `self.ok()`
-#
-#         pass
-#
-#     def about(self):
-#         """"""
-#         if self.__about_form is not None:
-#             self.__about_form.show()
-#         else:
-#             try:
-#                 self.__about_form = AboutDialog(fp_or_html=self.AppInfo.doc_html)
-#                 self.__about_form.show()
-#             except Exception as e:
-#                 self.statusBar().showMessage(str(e))
-#
-#     def keyPressEvent(self, event):
-#         if event.key() == QtCore.Qt.Key_Escape:
-#             self.close()
-#         elif event.key() == QtCore.Qt.Key_Return or event.key() == QtCore.Qt.Key_Enter:
-#             try:
-#                 getattr(self, 'ok')()
-#             except Exception as e:
-#                 raise e
-#         event.accept()
-#
-#     def __user_usage_stats(self):
-#         try:
-#             rp = post_to_knack_user_usage_stats(
-#                 # user indicator
-#                 user=str(getlogin()),
-#
-#                 # current app version
-#                 version=__version__,
-#
-#                 # datetime format following https://www.knack.com/developer-documentation/#type-date-time one line string
-#                 # example "03/28/2014 10:30pm"
-#                 date=datetime.now().strftime("%d%m%Y %H:%M%p"),
-#
-#                 # action is the current app id
-#                 content=self.__id
-#             )
-#
-#             logger.info(f'STATS POST STATUS {rp}')
-#             logger.debug(f'{rp.text}')
-#         except Exception as e:
-#             logger.error(f'User stats post failed {e}')
-#
 
 if __name__ == '__main__':
     pass
