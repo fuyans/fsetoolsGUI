67c9eb5f726223b6a113946587c1d2bfcf263e05
fuyans <fuyans@gmail.com>
2020-06-30 09:43:08 +0100

    Fixed an issue associated with multiprocessing by adding freeze_support.

--- buildscript/pyinstaller_build_entry.py
+++ buildscript/pyinstaller_build_entry.py
@@ -1,3 +1,10 @@
+from scipy.special import cython_special
+
+from fsetoolsGUI.gui.__main__ import *
+
+fake_modules = [cython_special]
+from multiprocessing import freeze_support
+
 if __name__ == '__main__':
-    from fsetoolsGUI.gui.__main__ import main
+    freeze_support()
     main()
