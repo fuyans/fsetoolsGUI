4d51f506b3be8f73a64fe1ffaa92580fa5d1e5ea
fuyans <fuyans@gmail.com>
2020-08-15 01:54:59 +0100

    Minor improvements.

--- buildscript/pyinstaller_build_entry.py
+++ buildscript/pyinstaller_build_entry.py
@@ -1,9 +1,10 @@
+from multiprocessing import freeze_support
+
 from scipy.special import cython_special
 
 from fsetoolsGUI.gui.__main__ import *
 
 fake_modules = [cython_special]
-from multiprocessing import freeze_support
 
 if __name__ == '__main__':
     freeze_support()
