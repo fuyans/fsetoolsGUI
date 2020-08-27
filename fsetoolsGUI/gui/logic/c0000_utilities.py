5457ed798489630996d5a59707ac83ae5ba9dc82
fuyans <fuyans@gmail.com>
2020-08-28 00:41:11 +0100

    Added ProgressBar.

--- fsetoolsGUI/gui/logic/c0000_utilities.py
+++ fsetoolsGUI/gui/logic/c0000_utilities.py
@@ -1,4 +1,4 @@
-from PySide2 import QtCore, QtGui
+from PySide2 import QtCore, QtGui, QtWidgets
 
 
 class Validator:
@@ -18,9 +18,64 @@ class Validator:
         return self.__signed_float
 
 
-def copy_to_clipboard(s: str):
-    clipboard = QtGui.QGuiApplication.clipboard()
-    clipboard.setText(s)
+class ProgressBar(QtWidgets.QDialog):
+    def __init__(self, title: str = None, initial_value: int = 0, parent=None):
+        super().__init__(parent=parent)
+        self.setWindowFlag(QtCore.Qt.WindowContextHelpButtonHint, False)
+
+        self.setWindowTitle(title)
+        self.setSizeGripEnabled(False)
+
+        # ==============
+        # instantiate ui
+        # ==============
+        self.progressbar = QtWidgets.QProgressBar()
+        self.progressbar.setMinimum(0), self.progressbar.setMaximum(100), self.progressbar.setValue(initial_value)
+        self.progress_label = QtWidgets.QLabel('--')
+
+        self.grid_layout = QtWidgets.QHBoxLayout()
+        self.grid_layout.addWidget(self.progressbar)
+        self.grid_layout.addWidget(self.progress_label)
+
+        self.setLayout(self.grid_layout)
+
+        # =========================
+        # instantiate signal object
+        # =========================
+        class __Signals(QtCore.QObject):
+            __process_complete = QtCore.Signal(bool)
+            __progress = QtCore.Signal(int)
+            __progress_label = QtCore.Signal(str)
+
+            @property
+            def complete(self) -> QtCore.Signal:
+                return self.__process_complete
+
+            @property
+            def progress(self) -> QtCore.Signal:
+                return self.__progress
+
+            @property
+            def progress_label(self) -> QtCore.Signal:
+                return self.__progress_label
+
+        self.Signals = __Signals()
+
+        # ======================
+        # assign signal and slot
+        # ======================
+        self.Signals.progress.connect(self.update_progress_bar)
+        self.Signals.progress_label.connect(self.update_progress_label)
+
+        self.resize(300, 50)
+
+    @QtCore.Slot(int)
+    def update_progress_bar(self, progress: int):
+        self.progressbar.setValue(progress)
+
+    @QtCore.Slot(str)
+    def update_progress_label(self, text: str):
+        self.progress_label.setText(text)
 
 
 class Counter:
@@ -60,6 +115,12 @@ class Counter:
     def value(self, v: int):
         assert isinstance(v, int)
         self.__v = v
+
+
+def copy_to_clipboard(s: str):
+    clipboard = QtGui.QGuiApplication.clipboard()
+    clipboard.setText(s)
+
 
 if __name__ == '__main__':
     counter = Counter()
