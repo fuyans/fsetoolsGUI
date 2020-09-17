3c5636ce6aa9becc38ae2f8f093d5b41429ba246
fuyans <fuyans@gmail.com>
2020-09-17 14:28:35 +0100

    Converted to QDialog.

--- fsetoolsGUI/gui/logic/custom_plot.py
+++ fsetoolsGUI/gui/logic/custom_plot.py
@@ -3,7 +3,7 @@ from os import path
 import matplotlib.pyplot as plt
 from PySide2 import QtCore
 from PySide2 import QtWidgets
-from PySide2.QtWidgets import QPushButton, QHBoxLayout, QSizePolicy, QWidget, QVBoxLayout, QFrame, QSpacerItem
+from PySide2.QtWidgets import QPushButton, QHBoxLayout, QSizePolicy, QVBoxLayout, QFrame, QSpacerItem
 
 import fsetoolsGUI
 
@@ -20,64 +20,62 @@ except ModuleNotFoundError:
     from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 
 
-class AppUI(object):
-
-    def setupUi(self, main_window):
-        self.centralwidget = QWidget(main_window)
-        self.p0_layout = QVBoxLayout(self.centralwidget)
-        self.p0_layout.setSpacing(0), self.p0_layout.setContentsMargins(15, 15, 15, 15)
-        self.p0_layout.setContentsMargins(5, 5, 5, 5)
-
-        self.frame = QFrame(self.centralwidget)
-        self.frame_layout = QVBoxLayout(self.frame)
-        self.frame_layout.setSpacing(0)
-        self.frame_layout.setContentsMargins(0, 0, 0, 0)
-
-        self.p0_layout.addWidget(self.frame)
-
-        self.p3_layout = QHBoxLayout()
-        self.refresh = QPushButton('Refresh', self.centralwidget)
-        self.refresh.setStyleSheet('padding-left:10px; padding-right:10px; padding-top:2px; padding-bottom:2px;')
-        self.save_figure = QPushButton('Save', self.centralwidget)
-        self.save_figure.setStyleSheet('padding-left:10px; padding-right:10px; padding-top:2px; padding-bottom:2px;')
-
-        self.p3_layout.addItem(QSpacerItem(2, 2, QSizePolicy.Expanding, QSizePolicy.Minimum))
-        self.p3_layout.addWidget(self.refresh)
-        self.p3_layout.addSpacing(5)
-        self.p3_layout.addWidget(self.save_figure)
-        self.p3_layout.addSpacing(15)
-
-        self.p0_layout.addLayout(self.p3_layout)
-
-        main_window.setCentralWidget(self.centralwidget)
-
-
-class App(QtWidgets.QMainWindow):
-
+class App(QtWidgets.QDialog):
     def __init__(self, parent=None, title: str = None, show_toolbar: bool = False):
 
         super().__init__(parent=parent)
-        self.ui = AppUI()
-        self.ui.setupUi(self)
 
+        self.setWindowFlag(QtCore.Qt.WindowCloseButtonHint, True)
+        self.setWindowFlag(QtCore.Qt.WindowMinimizeButtonHint, True)
+        self.setWindowFlag(QtCore.Qt.WindowMaximizeButtonHint, True)
+
+        # ======================
+        # instantiate UI objects
+        # ======================
         self.setStyleSheet(qt_css)
 
         if title:
             self.setWindowTitle(title)
 
-        # instantiate figure and associated objects
+        self.p0_layout = QVBoxLayout()
+        self.p0_layout.setSpacing(0), self.p0_layout.setContentsMargins(0, 0, 0, 10)
+        self.frame = QFrame()
+        self.frame_layout = QVBoxLayout(self.frame)
+        self.frame_layout.setSpacing(0)
+        self.frame_layout.setContentsMargins(0, 0, 0, 0)
+        self.p0_layout.addWidget(self.frame)
+
+        self.p3_layout = QHBoxLayout()
+        self.p3_refresh = QPushButton('Refresh')
+        self.p3_refresh.setStyleSheet('padding-left:10px; padding-right:10px; padding-top:2px; padding-bottom:2px;')
+        self.p3_save_figure = QPushButton('Save')
+        self.p3_save_figure.setStyleSheet('padding-left:10px; padding-right:10px; padding-top:2px; padding-bottom:2px;')
+
+        self.p3_layout.addItem(QSpacerItem(2, 2, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        self.p3_layout.addWidget(self.p3_refresh)
+        self.p3_layout.addSpacing(5)
+        self.p3_layout.addWidget(self.p3_save_figure)
+        self.p3_layout.addSpacing(15)
+
+        self.p0_layout.addLayout(self.p3_layout)
+
+        self.setLayout(self.p0_layout)
+
+        # =========================================
+        # Instantiate figure and associated objects
+        # =========================================
         self.figure = plt.figure()
         self.figure.patch.set_facecolor('None')
 
         self.figure_canvas = FigureCanvas(self.figure)
         self.figure_canvas.setStyleSheet("background-color:transparent;border:0px")  # set background transparent.
-        self.ui.frame_layout.addWidget(self.figure_canvas)
+        self.frame_layout.addWidget(self.figure_canvas)
         if show_toolbar:
             self.toolbar = NavigationToolbar(self.figure_canvas, self)
-            self.ui.frame_layout.addWidget(self.toolbar)
+            self.frame_layout.addWidget(self.toolbar)
 
-        self.ui.refresh.clicked.connect(self.refresh_figure)
-        self.ui.save_figure.clicked.connect(self.save_figure)
+        self.p3_refresh.clicked.connect(self.refresh_figure)
+        self.p3_save_figure.clicked.connect(self.save_figure)
 
         self.resize(380, 380)
 
@@ -110,10 +108,11 @@ if __name__ == '__main__':
     import sys
 
     qapp = QtWidgets.QApplication(sys.argv)
-    app = App()
+    app = App(title='Example')
     app.show()
 
     ax = app.add_subplots()
+    ax.fill_between([0, 1], [0, 1], [0, 0.5], facecolor='grey', alpha=0.2, label='area between lines')
     ax.plot([0, 1], [0, 1], label='line 1')
     ax.plot([0, 1], [0, 0.5], label='line 2')
     ax.set_xlabel('x label', fontsize='small')
