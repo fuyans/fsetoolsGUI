0b28e2c3e2a903c7c3f60e983605063d94d8a6c4
fuyans <fuyans@gmail.com>
2020-10-01 01:40:56 +0100

    Improved existing table module and added `TableApp2`.

--- fsetoolsGUI/gui/logic/custom_table.py
+++ fsetoolsGUI/gui/logic/custom_table.py
@@ -5,6 +5,8 @@ from typing import Union
 
 from PySide2 import QtGui, QtCore, QtWidgets
 
+from fsetoolsGUI import logger
+
 
 class MyDelegate(QtWidgets.QItemDelegate):
 
@@ -26,29 +28,15 @@ class MyDelegate(QtWidgets.QItemDelegate):
         editor.setText(str(val))
 
 
-class TableWindow(QtWidgets.QDialog):
-    def __init__(
-            self,
-            content: list = None,
-            col_headers: list = None,
-            row_headers: list = None,
-            window_title: str = None,
-            window_geometry: Union[tuple, list, QtCore.QRect] = (),
-            enable_sorting: bool = True,
-            parent=None,
-            *args,
-    ):
-        # =================
-        # instantiate super
-        # =================
-        super().__init__(parent, *args)
+class TableBaseClass(QtWidgets.QDialog):
+    def __init__(self, parent=None, window_geometry: QtCore.QRect = None, window_title: str = None):
+        super().__init__(parent=parent)
 
-        # =================
         # set ui properties
-        # =================
         self.setWindowFlag(QtCore.Qt.WindowCloseButtonHint, True)
         self.setWindowFlag(QtCore.Qt.WindowMinimizeButtonHint, True)
         self.setWindowFlag(QtCore.Qt.WindowMaximizeButtonHint, True)
+
         if window_geometry:
             if isinstance(window_geometry, QtCore.QRect):
                 self.setGeometry(window_geometry)
@@ -57,34 +45,7 @@ class TableWindow(QtWidgets.QDialog):
         if window_title:
             self.setWindowTitle(window_title)
 
-        # =================
-        # create ui objects
-        # =================
-        self.label_tip = QtWidgets.QLabel()
-        self.label_tip.setText('Ctrl+A to select all, Ctrl+C to copy selected cells.')
-        self.label_tip.wordWrap()
-
-        delegate = MyDelegate()
-        self.TableModel = TableModel(self)
-        self.TableView = QtWidgets.QTableView()
-        if content is not None:
-            self.TableModel.update_model(content_data=content, row_headers=row_headers, col_headers=col_headers)
-        self.TableView.setModel(self.TableModel)
-        self.TableView.setItemDelegate(delegate)
-
-        layout = QtWidgets.QVBoxLayout(self)
-        layout.addWidget(self.TableView)
-        layout.addWidget(self.label_tip)
-        self.setLayout(layout)
-
-        # default values
-
-        self.refresh_content_size(enable_sorting=enable_sorting)
-
     def refresh_content_size(self, enable_sorting: bool = True):
-        # ====================
-        # set table properties
-        # ====================
         # set font
         # font = QtGui.QFont("Courier New", 10)
         # self.TableView.setFont(font)
@@ -102,12 +63,10 @@ class TableWindow(QtWidgets.QDialog):
         self.refresh_content_size()
 
     def copy_selection(self):
-
         # get selection index
         selection = self.TableView.selectedIndexes()
 
         if selection:
-
             # create table data container
             rows = sorted(index.row() for index in selection)
             cols = sorted(index.column() for index in selection)
@@ -125,7 +84,6 @@ class TableWindow(QtWidgets.QDialog):
             stream = io.StringIO()
             csv.writer(stream, delimiter='\t').writerows(table)
             QtGui.QClipboard().setText(stream.getvalue())
-
         return
 
     def keyPressEvent(self, event):
@@ -134,6 +92,127 @@ class TableWindow(QtWidgets.QDialog):
         elif QtGui.QKeySequence(event.key() + int(event.modifiers())) == QtGui.QKeySequence('Ctrl+C'):
             self.copy_selection()
         event.accept()
+
+    def table_insert(self, TableModel, TableView: QtWidgets.QTableView):
+        # get selected row index
+        selected_indexes = TableView.selectionModel().selectedIndexes()
+        selected_row_index = selected_indexes[-1].row()
+        logger.debug(f'Insert after row index {selected_row_index}')
+        # insert
+        TableModel.insertRow(selected_row_index + 1)
+        TableView.resizeRowsToContents()
+        TableView.model().layoutChanged.emit()
+        # self.repaint()
+
+    def table_remove(self, TableModel, TableView: QtWidgets.QTableView):
+        if TableModel.rowCount(TableView) <= 1:
+            logger.error('No row to delete')
+            raise ValueError('Not enough rows to delete.')
+
+        # get selected row index
+        selected_indexes = TableView.selectionModel().selectedIndexes()
+        if len(selected_indexes) > 1:
+            selected_row_index = selected_indexes[-1].row()
+        else:
+            selected_row_index = selected_indexes[0].row()
+        logger.debug(f'Delete row {selected_row_index}')
+        # remove
+        TableModel.removeRow(selected_row_index)
+        TableView.resizeRowsToContents()
+        TableView.model().layoutChanged.emit()
+        # self.repaint()
+
+
+class TableWindow(TableBaseClass):
+    def __init__(
+            self,
+            content: list = None,
+            col_headers: list = None,
+            row_headers: list = None,
+            window_title: str = None,
+            window_geometry: Union[tuple, list, QtCore.QRect] = (),
+            enable_sorting: bool = True,
+            parent=None,
+            *args,
+    ):
+        super().__init__(parent, window_title=window_title, window_geometry=window_geometry)
+
+        # set ui properties
+        if window_geometry:
+            if isinstance(window_geometry, QtCore.QRect):
+                self.setGeometry(window_geometry)
+            elif isinstance(window_geometry, list) or isinstance(window_geometry, tuple):
+                self.setGeometry(*window_geometry)
+        if window_title:
+            self.setWindowTitle(window_title)
+
+        # create ui objects
+        self.label_tip = QtWidgets.QLabel()
+        self.label_tip.setText('Ctrl+A to select all, Ctrl+C to copy selected cells.')
+        self.label_tip.wordWrap()
+
+        delegate = MyDelegate()
+        self.TableModel = TableModel(self)
+        self.TableView = QtWidgets.QTableView()
+        if content is not None:
+            self.TableModel.update_model(content_data=content, row_headers=row_headers, col_headers=col_headers)
+        self.TableView.setModel(self.TableModel)
+        self.TableView.setItemDelegate(delegate)
+
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.TableView)
+        layout.addWidget(self.label_tip)
+        self.setLayout(layout)
+
+        self.refresh_content_size(enable_sorting=enable_sorting)
+
+
+class TableApp2(TableBaseClass):
+    def __init__(
+            self,
+            parent=None,
+            content_data: list = None,
+            row_headers: list = None,
+            col_headers: list = None,
+            enable_sorting: bool = True,
+            window_title: str = None,
+            window_geometry: Union[tuple, list, QtCore.QRect] = ()
+    ):
+        super().__init__(parent=parent, window_title=window_title)
+
+        self.in_insert_row = QtWidgets.QPushButton('+')
+        self.in_insert_row.setToolTip('Insert a row below the highlighted cell')
+        self.in_delete_row = QtWidgets.QPushButton('-')
+        self.in_delete_row.setToolTip('Delete a row below the highlighted cell')
+        layout_1 = QtWidgets.QHBoxLayout()
+        layout_1.setContentsMargins(0, 0, 0, 0), layout_1.setSpacing(0)
+        layout_1.addStretch(1)
+        layout_1.addWidget(self.in_insert_row)
+        layout_1.addWidget(self.in_delete_row)
+
+        # create ui objects
+        label_tip = QtWidgets.QLabel('Ctrl+A to select all, Ctrl+C to copy selected cells.')
+        label_tip.wordWrap()
+
+        delegate = MyDelegate()
+        self.TableModel = TableModel(self)
+        self.TableView = QtWidgets.QTableView()
+        if content_data is not None:
+            self.TableModel.update_model(content_data=content_data, row_headers=row_headers, col_headers=col_headers)
+        self.TableView.setModel(self.TableModel)
+        self.TableView.setItemDelegate(delegate)
+
+        layout = QtWidgets.QVBoxLayout(self)
+        # layout.addWidget(layout_1)
+        layout.addLayout(layout_1)
+        layout.addWidget(self.TableView)
+        layout.addWidget(label_tip)
+        self.setLayout(layout)
+
+        self.in_insert_row.clicked.connect(lambda: self.table_insert(self.TableModel, self.TableView))
+        self.in_delete_row.clicked.connect(lambda: self.table_remove(self.TableModel, self.TableView))
+
+        self.refresh_content_size(enable_sorting=enable_sorting)
 
 
 class TableModel(QtCore.QAbstractTableModel):
@@ -240,14 +319,12 @@ class TableModel(QtCore.QAbstractTableModel):
     def insertRow(self, position, index=QtCore.QModelIndex()):
         self.beginInsertRows(QtCore.QModelIndex(), position, position - 1)
         self.content.insert(position, [''] * len(self.content[0]))
-        print(self.content)
         self.endInsertRows()
         return True
 
 
-if __name__ == '__main__':
+def _test():
     # the solvent data ...
-    col_header = ['Solvent Name', ' BP (deg C)', ' MP (deg C)', ' Density (g/ml)']
     row_header = ['1', '2', '3', '4']
     # use numbers for numeric data to sort properly
     content = [
@@ -258,7 +335,7 @@ if __name__ == '__main__':
     ]
 
     app = QtWidgets.QApplication([])
-    win = TableWindow(content, col_headers=['col 1', 'col 2', 'col 3', 'col 4'], row_headers=['row 1', 'row 2', 'row 3', 'row 4'])
+    win = TableApp2(content, col_headers=['col 1', 'col 2', 'col 3', 'col 4'], row_headers=['row 1', 'row 2', 'row 3', 'row 4'])
 
     content = [
         [21, 12, 13, 14],
@@ -271,3 +348,25 @@ if __name__ == '__main__':
 
     win.show()
     app.exec_()
+
+
+def _test_TableApp2_1():
+    # the solvent data ...
+    # use numbers for numeric data to sort properly
+    content = [
+        [11, 12, 13, 14],
+        [21, 22, 23, 24],
+        [31, 32, 33, 34],
+        [41, 42, 43, 44],
+    ]
+
+    app = QtWidgets.QApplication([])
+    win = TableApp2()
+    win.update_table_content(content, col_headers=['col 1', 'col 2', 'col 3', 'col 4'], row_headers=['row 1', 'row 2', 'row 3', 'row 4'])
+
+    win.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    _test_TableApp2_1()
