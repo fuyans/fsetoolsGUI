69e16fff6160ae0ddc78b78eef777d909c774053
fuyans <fuyans@gmail.com>
2020-12-27 06:04:29 +0000

    Fix non-incompatibility between Mac OS Big Sur and Pyside2 where app do not show

--- buildscript/pyinstaller_build_entry.py
+++ buildscript/pyinstaller_build_entry.py
@@ -2,9 +2,20 @@ if __name__ == '__main__':
     import multiprocessing
 
     multiprocessing.freeze_support()
+
+    fake_modules = list()
     from scipy.special import cython_special
 
-    fake_modules = [cython_special]
+    fake_modules.append(cython_special)
+
+    from sys import platform
+
+    if platform == "darwin":
+        # OS X
+        import _tkinter
+
+        fake_modules.append(_tkinter)
+
     from fsetoolsGUI.gui.__main__ import *
 
     main()
