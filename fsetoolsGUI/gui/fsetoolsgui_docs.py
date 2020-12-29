8a23f8634a6767a0d78ab49e231679521645ea3d
fuyans <fuyans@gmail.com>
2020-12-29 22:45:44 +0000

    Fixed docs url not open on Mac OS.

--- fsetoolsGUI/gui/fsetoolsgui_docs.py
+++ fsetoolsGUI/gui/fsetoolsgui_docs.py
@@ -1,5 +1,7 @@
+import os
+import subprocess
+import sys
 import webbrowser
-from os.path import join
 
 from fsetoolsGUI import __root_dir__, logger
 
@@ -8,14 +10,22 @@ class App:
     app_name_short = 'Documentation'
     app_name_long = 'Documentation'
 
-    url = join(__root_dir__, 'docs', 'html', 'index.html')
+    url = os.path.join(__root_dir__, 'docs', 'html', 'index.html')
 
     def __init__(self, *args, **kwargs):
         pass
 
     def show(self):
-        logger.info(f'Opening docs url, {self.url}')
-        webbrowser.open(self.url)
+        logger.info(f'Opening docs url: {self.url}')
+        try:
+            if sys.platform == 'darwin':
+                # webbrowser does not work in the latest MAC OS update, Big Sur. Fall back to subprocess.Popen to open url. 29/12/2020
+                subprocess.Popen(['open', self.url])
+            else:
+                webbrowser.open(self.url)
+
+        except Exception as e:
+            logger.info(f'Failed to open docs url, {e}')
 
 
 if __name__ == '__main__':
